2f5faca058e18848daeb8f05053cfd8c817cb4e8
RobinROAR <zrb915@live.com>
2015-05-20 15:16:27 +0800

    add new data

---
+++ ReadFixedData.py
@@ -0,0 +1,50 @@
+﻿#encoding = utf-8
+import csv
+import MySQLdb
+		
+#read CSV
+def readFromCsv(filename):
+	
+	all_pos=[]
+	csvFile=file(filename,'rb')
+	reader=csv.reader(csvFile)
+	for line in reader:
+		if reader.line_num == 1:  
+			continue  		
+		pos = [line[0],line[1],line[2],line[3]]                             
+		all_pos.append(pos)
+			
+	print('all_pos:'+str(len(all_pos)))
+	return all_pos
+
+conn = MySQLdb.connect(host='localhost',user = 'root', passwd = '123456')
+conn.select_db('GPStemp')
+cursor = conn.cursor()
+
+# 从ｃｓｖ读取数据
+result = readFromCsv('birdDataLG.csv')
+cnt = 0
+#~ #写入数据库
+for i in result:
+    sql = 'insert into gps_bh(animal,longitude,latitude,date_time) values(%s,%s,%s,%s)'
+    param = (i[0],i[1],i[2],i[3])
+    n =cursor.execute(sql,param)
+    cnt+=n
+
+conn.commit()
+
+
+print '存储完毕'
+#查询插入情况
+sql = 'select * from gps_bh'
+n =cursor.execute(sql)
+for row in cursor.fetchall():
+	print row
+
+	
+cursor.close()
+conn.close()
+
+
+	
+	
--- test.py
+++ test.py
@@ -1,8 +1,68 @@
-import Image,os
+﻿import MySQLdb
+import math
+import csv
+import os,sys,time,gdal
+import random
+import scipy.stats as st
 
-image = image.convert("L")
+from gdalconst import *
 
-image.save("1.JPEG","JPEG")
+#calculate the result
+sumU=6640
+numU=[3994,2504,2,140]
+freU=[0.602,0.377,0.000,0.021]
+numA=[2170,4366,26,41]
+freA=[0.329,0.662,0.004,0.006]
+sumA=6603
 
-		
-		
+
+Z=st.norm.ppf(1-0.05/len(numU))
+wiList=[]                                       #value of   ui/ai
+seWiList=[]                                    #s.e value of wi
+interval=[]
+chi2= 0
+selection=['+','-','0']
+
+chi21=0
+interval1=[]
+dif = 0
+sq = 0
+print '----------------------------------------------------'
+print 'log-likehood Chi-squre test   +++++++++++++'
+for i in range(4):
+	#对数似然卡方检验
+	if(numU[i]==0):
+		numU[i]=1
+		freU[i]=float(1.0/sumU)
+	if(numA[i]==0):
+		numA[i]=1
+		freA[i]=float(1.0/sumA)
+
+	wiTemp =float(freU[i]/freA[i])
+	wiList.append(wiTemp)	
+	seTemp = wiTemp*float(math.sqrt(1.0/numU[i]-1.0/sumU+1.0/numA[i]-1.0/sumA))  #function use?
+
+	seWiList.append(seTemp)
+
+	intervalTemp = [wiTemp-Z*seTemp,wiTemp+Z*seTemp]
+	interval.append(intervalTemp)
+	chi2T = float(numU[i])*math.log(float(numU[i])/( float(numU[i]+numA[i])*sumU/(sumU+sumA)))+float(numA[i])*math.log(float(numA[i])/( float(numU[i]+numA[i])*sumA/(sumU+sumA)))
+	chi2+= chi2T*2
+	#~ chi2T = float(numU[i])*math.log(float(numU[i])/(numA[i]))
+	#~ chi2+= chi2T*2
+
+	#selection jugement
+	if(intervalTemp[0]>1):
+		a = selection[0]
+	else:
+		if(intervalTemp[1]<1):
+			a=selection[1]
+		else:
+			a=selection[2]	
+
+	print 'Category%2d:   Wi:%-9.3f s.e.(Wi):%9.3f   interval:(%9.3f   -- %9.3f)   selection:%4s'  %  (i+1,wiTemp,seTemp,intervalTemp[0],intervalTemp[1],a)
+
+
+print ''
+print ' chi2:%-20.3f df=%2d  P =%9.5F' % (chi2 ,len(numA)-1,1-float(st.chi2.cdf(chi2,len(numA)-1)))
+print ''
